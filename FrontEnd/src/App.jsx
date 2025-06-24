287e27118f9a66a82162bd2b10a633b7c4bd97d5
hassanq707 <qadrihassan25@gmail.com>
2025-06-24 21:18:11 +0500

    new_error

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,31 +1,29 @@
-import React from "react";
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import EmpDash from "./dashboard/EmpDash";
 import AdminDash from "./dashboard/AdminDash";
-import Leave from "./empCompo/Leave"
-import Attendance from "./empCompo/Attendance"
+import Leave from "./empCompo/Leave";
+import Attendance from "./empCompo/Attendance";
 import Req_Attendance from "./adminCompo/Req_Attendance";
 import Emp_Status from "./adminCompo/Emp_Status";
-import WorkStatus from "./empCompo/WorkStatus"; 
-
+import WorkStatus from "./empCompo/WorkStatus";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
+    <Route>
       <Route path="/" element={<EmpDash />}>
-        <Route path="" element={<Attendance />}/>
-        <Route path="leave" element={<Leave />}/>
-        <Route path="status" element={<WorkStatus />}/>
+        <Route index element={<Attendance />} />   
+        <Route path="leave" element={<Leave />} />
+        <Route path="status" element={<WorkStatus />} />
       </Route>
       <Route path="/admin" element={<AdminDash />}>
-         <Route path="" element={<Req_Attendance />}/>
-         <Route path="Emp_Status" element={<Emp_Status />}/>
+        <Route index element={<Req_Attendance />} />
+        <Route path="Emp_Status" element={<Emp_Status />} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-    </>
+    </Route>
   )
 );
 
